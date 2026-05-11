37e8d78bba12345c4875e939e7ce0883615fdafb
Brayan Muñoz <ssarri08@gmail.com>
2026-05-10 22:07:18 -0500

    Proyecto tienda virtual CriolloMarket

---
+++ app/src/main/java/com/example/criollomarket/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.criollomarket;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.EdgeToEdge;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+}
